74a0ec5c1f03aabd043930b6fd1d980bb48ab73e
gautamb97 <www.gautam971997@gmail.com>
2020-12-14 09:18:17 +0530

    Modified:added_a_new_person_in_addressbook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Set;
 public class AddressBookMain {
     public static void main(String[] args){
         System.out.println("Welcome to Address Book");
+        Person person_deatils = new Person();
+        person_deatils.addPersonInfo();
     }
 }
 //Creating a class for person
@@ -11,7 +17,7 @@ class Person{
     String city;
     String state;
     int zip;
-    int phoneNumber;
+    long phoneNumber;
 
     public String getFirstName() {
         return firstName;
@@ -55,11 +61,40 @@ class Person{
     public void setZip(int zip) {
         this.zip = zip;
     }
-    public int getPhoneNumber() {
+    public long getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+    //Adding Person in Address book
+    public void addPersonInfo(){
+        Scanner personDetails = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+        setFirstName(personDetails.next());
+        System.out.println("Enter your last name: ");
+        setLastName(personDetails.next());
+        System.out.println("Enter your address: ");
+        setAddress(personDetails.nextLine());
+        setAddress(personDetails.nextLine());
+        System.out.println("Enter your city: ");
+        setCity(personDetails.next());
+        System.out.println("Enter your State: ");
+        setState(personDetails.next());
+        System.out.println("Enter your Zip Code: ");
+        setZip(personDetails.nextInt());
+        System.out.println("Enter your phone number: ");
+        setPhoneNumber(personDetails.nextInt());
+        List<String> personDetailsSet = new ArrayList<>();
+            personDetailsSet.add("Name= "+getFirstName()+" "+getLastName());
+            personDetailsSet.add("Address= "+getAddress());
+            personDetailsSet.add("City= "+getCity());
+            personDetailsSet.add("State= "+getState());
+            personDetailsSet.add("Zip Code= "+getZip());
+            personDetailsSet.add("Phone Number= "+getPhoneNumber());
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        System.out.println(personDetailsSet);
+        System.out.println("--------------------------------------------------------------------------------------------------");
     }
 }
